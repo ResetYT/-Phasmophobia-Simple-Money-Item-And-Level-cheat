9e6c904bbe04b4204cc9c9690b55d98d288a74e1
ResetYT <36563646+ResetYT@users.noreply.github.com>
2020-10-08 20:34:22 +0100

    Add files via upload

---
+++ Cheat.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using OPS.AntiCheat.Detector;
+using System;
+using System.Threading;
+using OPS.AntiCheat;
+using System.Windows.Forms;
+using ExitGames.Demos.DemoPunVoice;
+using UnityEngine.InputSystem;
+using System.IO;
+using System.Collections.Generic;
+
+namespace YEP
+{
+    public class Cheat : MonoBehaviour
+    {
+        #region Standard Unity methods
+        // Runs once.
+        private void Start()
+        {
+            Utils.CreateConsole();
+
+
+            {
+                /* Player Stats
+                 * Change the number 50 in myTotalExp and totalExp to the number you want for your level
+                 * Change PlayersMoney to what you want
+                 * Change Item Amounts Like The Example Below
+                 * FileBasedPrefs.SetInt("EMFReaderInventory", 10);
+                */
+                FileBasedPrefs.SetInt("myTotalExp", 50 * 100);
+                FileBasedPrefs.SetInt("totalExp", 50 * 100);
+                FileBasedPrefs.SetInt("PlayersMoney", 50000);
+                FileBasedPrefs.SetInt("completedTraining", 1);
+                FileBasedPrefs.SetInt("PlayerDied", 0);
+                
+                // Items
+                FileBasedPrefs.SetInt("EMFReaderInventory", int.MaxValue);
+                FileBasedPrefs.SetInt("FlashlightInventory", int.MaxValue);
+                FileBasedPrefs.SetInt("CameraInventory", int.MaxValue);
+                FileBasedPrefs.SetInt("LighterInventory", int.MaxValue);
+                FileBasedPrefs.SetInt("CandleInventory", int.MaxValue);
+                FileBasedPrefs.SetInt("UVFlashlightInventory", int.MaxValue);
+                FileBasedPrefs.SetInt("CrucifixInventory", int.MaxValue);
+                FileBasedPrefs.SetInt("DSLRCameraInventory", int.MaxValue);
+                FileBasedPrefs.SetInt("EVPRecorderInventory", int.MaxValue);
+                FileBasedPrefs.SetInt("SaltInventory", int.MaxValue);
+                FileBasedPrefs.SetInt("TripodInventory", int.MaxValue);
+                FileBasedPrefs.SetInt("StrongFlashlightInventory", int.MaxValue);
+                FileBasedPrefs.SetInt("MotionSensorInventory", int.MaxValue);
+                FileBasedPrefs.SetInt("SoundSensorInventory", int.MaxValue);
+                FileBasedPrefs.SetInt("SanityPillsInventory", int.MaxValue);
+                FileBasedPrefs.SetInt("ThermometerInventory", int.MaxValue);
+                FileBasedPrefs.SetInt("GhostWritingBookInventory", int.MaxValue);
+                FileBasedPrefs.SetInt("IRLightSensorInventory", int.MaxValue);
+                FileBasedPrefs.SetInt("ParabolicMicrophoneInventory", int.MaxValue);
+                FileBasedPrefs.SetInt("IRLightSensorInventory", int.MaxValue);
+                FileBasedPrefs.SetInt("ParabolicMicrophoneInventory", int.MaxValue);
+                FileBasedPrefs.SetInt("GlowstickInventory", int.MaxValue);
+                FileBasedPrefs.SetInt("HeadMountedCameraInventory", int.MaxValue);
+                FileBasedPrefs.SetInt("SmudgeSticksInventory", int.MaxValue);
+            }
+
+            Console.WriteLine("YEP Cock");
+        }
+        #endregion
+
+        private Player MyPlayer => GameController.instance.myPlayer.player;
+
+        private List<PlayerData> Players => GameController.instance.playersData;
+
+    }
+}
---
+++ Utils.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YEP
+{
+    public class Utils
+    {
+        public static void CreateConsole()
+        {
+            AllocConsole();
+
+            IntPtr stdHandle = GetStdHandle(-11);
+            IntPtr intPtr = CreateFile("CONOUT$", 3221225472u, FileShare.Write, IntPtr.Zero, FileMode.OpenOrCreate, (FileAttributes)0, IntPtr.Zero);
+
+            if (intPtr != stdHandle)
+            {
+                SetStdHandle(-11, intPtr);
+
+                Console.SetOut(new StreamWriter(Console.OpenStandardOutput(), Console.OutputEncoding)
+                {
+                    AutoFlush = true
+                });
+            }
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr GetStdHandle(int nStdHandle);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool SetStdHandle(int nStdHandle, IntPtr hHandle);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern IntPtr CreateFile([MarshalAs(UnmanagedType.LPTStr)] string filename, [MarshalAs(UnmanagedType.U4)] uint access, [MarshalAs(UnmanagedType.U4)] FileShare share, IntPtr securityAttributes, [MarshalAs(UnmanagedType.U4)] FileMode creationDisposition, [MarshalAs(UnmanagedType.U4)] FileAttributes flagsAndAttributes, IntPtr templateFile);
+
+        [DllImport("kernel32")]
+        private static extern bool AllocConsole();
+    }
+}
